37820916f1d4d1241db3b7a58d2a0528fe096acf
LucasNGRE <lucas12negre@gmail.com>
2024-07-16 11:53:26 +0200

    add task 7

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,26 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+
+    if (typeof name !== 'string') {
+      throw new Error('Name is required');
+    }
+
+    if (typeof code !== 'string') {
+      throw new Error('Code is required');
+    }
+  }
+
+  getName() {
+    return this._name;
+  }
+
+  getCode() {
+    return this._code;
+  }
+
+  toString() {
+    return `[${this._code}]`;
+  }
+}
