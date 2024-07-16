42aab6ca9fa1dc959fbcfec84325241dd4cbd052
LucasNGRE <lucas12negre@gmail.com>
2024-07-16 14:22:48 +0200

    modify the task 10 to use reflect

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -3,8 +3,22 @@ export default class Car {
         this._brand = brand;
         this._motor = motor;
         this._color = color;
-        }
+    }
+
+    get brand() {
+        return this._brand;
+    }
+
+    get motor() {
+        return this._motor;
+    }
+
+    get color() {
+        return this._color;
+    }
+
     cloneCar() {
-        return new Car(this._brand, this._motor, this._color);
+        const constructor = Reflect.getPrototypeOf(this).constructor;
+        return new constructor(this._brand, this._motor, this._color);
     }
 }
