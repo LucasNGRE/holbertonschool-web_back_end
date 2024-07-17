9b6b60a02e90a5f3fff06c1b5de302e92dbf436a
LucasNGRE <lucas12negre@gmail.com>
2024-07-17 11:53:02 +0200

    eslint

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,10 +1,10 @@
-import Classroom from './0-classroom';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-    const rooms = [
-        new Classroom(19),
-        new Classroom(20),
-        new Classroom(34),
-    ];
-    return rooms;
+  const rooms = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
+  return rooms;
 }
