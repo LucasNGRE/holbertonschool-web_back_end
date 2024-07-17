a08d31614d057e452733527cca611a56006d8326
LucasNGRE <lucas12negre@gmail.com>
2024-07-17 14:49:44 +0200

    modify the task

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -4,6 +4,10 @@ export default class Building {
       throw new TypeError('Sqft must be a number');
     }
     this._sqft = sqft;
+    
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
