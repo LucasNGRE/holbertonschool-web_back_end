8548b295a3fe6e47a3b5ceff4c7bbb1dcc630822
LucasNGRE <lucas12negre@gmail.com>
2024-07-17 14:25:22 +0200

    eslint

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -19,15 +19,15 @@ export default class Currency {
       return this._code;
     }
   
-    get name() {
-      return this._name;
-    }
-  
     set code(newCode) {
       if (typeof newCode !== 'string') {
         throw new TypeError('Code must be a string');
       }
       this._code = newCode;
+    }
+  
+    get name() {
+      return this._name;
     }
   
     set name(newName) {
