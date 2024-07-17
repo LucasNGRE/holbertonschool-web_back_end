f53c3928f725c6af5aa29e02e58a0a67e733bda9
LucasNGRE <lucas12negre@gmail.com>
2024-07-17 15:04:47 +0200

    modified

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,48 +1,25 @@
 /* eslint-disable */
-export class HolbertonClass {
-    constructor(year, location) {
-      this._year = year;
-      this._location = location;
+export default class Car {
+    constructor(brand, motor, color) {
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
     }
-  
-    get year() {
-      return this._year;
+
+    get brand() {
+        return this._brand;
     }
-  
-    get location() {
-      return this._location;
+
+    get motor() {
+        return this._motor;
     }
-  }
-  
-  export class StudentHolberton {
-    constructor(firstName, lastName, holbertonClass) {
-      this._firstName = firstName;
-      this._lastName = lastName;
-      this._holbertonClass = holbertonClass;
+
+    get color() {
+        return this._color;
     }
-  
-    get fullName() {
-      return `${this._firstName} ${this._lastName}`;
+
+    cloneCar() {
+        const constructor = Reflect.getPrototypeOf(this).constructor;
+        return new constructor(this._brand, this._motor, this._color);
     }
-  
-    get holbertonClass() {
-      return this._holbertonClass;
-    }
-  
-    get fullStudentDescription() {
-      return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
-    }
-  }
-  
-  const class2019 = new HolbertonClass(2019, 'San Francisco');
-  const class2020 = new HolbertonClass(2020, 'San Francisco');
-  
-  const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-  const student2 = new StudentHolberton('John', 'Doe', class2020);
-  const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-  const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-  const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
-  
-  const listOfStudents = [student1, student2, student3, student4, student5];
-  
-  export default listOfStudents;
+}
