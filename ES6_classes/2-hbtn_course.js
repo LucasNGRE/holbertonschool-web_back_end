3c424fe53a43202276e2db35805baa077f3c929b
LucasNGRE <lucas12negre@gmail.com>
2024-07-17 11:56:37 +0200

    modify setters

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -19,23 +19,32 @@ export default class HolbertonCourse {
       return this._name;
     }
   
+    set name(newName) {
+      if (typeof newName !== 'string') {
+        throw new TypeError('Name must be a string');
+      }
+      this._name = newName;
+    }
+  
     get length() {
       return this._length;
+    }
+  
+    set length(newLength) {
+      if (typeof newLength !== 'number') {
+        throw new TypeError('Length must be a number');
+      }
+      this._length = newLength;
     }
   
     get students() {
       return this._students;
     }
   
-    set name(newName) {
-      this._name = newName;
-    }
-  
-    set length(newLength) {
-      this._length = newLength;
-    }
-  
     set students(newStudents) {
+      if (!Array.isArray(newStudents) || !newStudents.every(student => typeof student === 'string')) {
+        throw new TypeError('Students must be an array of strings');
+      }
       this._students = newStudents;
     }
   }
