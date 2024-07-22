fc9a13dd6d7dea4c3b07f8f229ff88baba2c4a31
LucasNGRE <lucas12negre@gmail.com>
2024-07-22 13:45:56 +0200

    modify

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,7 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
-    .filter((student) => student.location === city).map((student) => {
+    .filter((student) => student.location === city)
+    .map((student) => {
       const updatedGrade = newGrades.find((grade) => grade.studentId === student.id);
       if (updatedGrade) {
         return { ...student, grade: updatedGrade.grade };
