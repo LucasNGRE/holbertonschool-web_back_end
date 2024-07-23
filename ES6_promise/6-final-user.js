e10f2eb2ec80eeee9fbf7a0161b7adbb6bd16a73
LucasNGRE <lucas12negre@gmail.com>
2024-07-23 10:28:05 +0200

    modify

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,13 +1,12 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
-  const user = signUpUser(firstName, lastName);
-  const photo = uploadPhoto(filename);
-
-  return Promise.allSettled([user, photo])
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+  return Promise.allSettled([userPromise, photoPromise])
     .then((results) => results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.result : result.reason,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
     })));
 }
