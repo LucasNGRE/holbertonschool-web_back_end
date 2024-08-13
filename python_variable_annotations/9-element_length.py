3a0c97a406fa30364c8c7c13cec79e970dc4128c
LucasNGRE <lucas12negre@gmail.com>
2024-08-13 09:52:53 +0200

    modify

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -6,8 +6,3 @@ from typing import Iterable, Sequence, List, Tuple
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     return [(i, len(i)) for i in lst]
-
-
-if __name__ == "__main__":
-    element_length = __import__('9-element_length').element_length
-    print(element_length.__annotations__)
