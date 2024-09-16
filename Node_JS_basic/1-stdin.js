8ae46dedb21e147eb7cb24d4e8987af8dccbabcf
LucasNGRE <lucas12negre@gmail.com>
2024-09-16 09:54:11 +0200

    modify

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,24 +1,16 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Créer une interface de lecture pour la saisie utilisateur
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-// Afficher le message de bienvenue
-console.log('Welcome to Holberton School, what is your name?');
-
-// Lire la réponse de l'utilisateur
-rl.question('', (name) => {
-  // Afficher le nom de l'utilisateur
-  console.log(`Your name is: ${name}`);
-
-  // Fermer l'interface de lecture
-  rl.close();
-});
-
-// Afficher un message de fermeture lorsque le programme se termine
-rl.on('close', () => {
-  console.log('This important software is now closing');
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
