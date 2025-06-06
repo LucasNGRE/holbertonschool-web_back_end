e0abb9aa2db530830f286215d004ce314960fbb4
LucasNGRE <lucas12negre@gmail.com>
2025-06-06 13:50:31 +0200

    6. Basic auth

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module for basic authentication using Flask."""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic authentication class that extends the Auth class."""
+    pass
