3495c34137d11d23ca7e73f4e2134a337ec059f5
LucasNGRE <lucas12negre@gmail.com>
2025-06-06 14:43:51 +0200

    9. Basic - User credentials

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -48,3 +48,17 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+        self,
+        decoded_base64_authorization_header: str
+    ) -> (str, str):
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        try:
+            user_info = decoded_base64_authorization_header.split(":")
+            return user_info[0], user_info[1]
+        except Exception:
+            return None, None
