bc9f1c173577f3f6eda2ff49c3432b84a0a5bee0
LucasNGRE <lucas12negre@gmail.com>
2025-06-06 13:33:03 +0200

    5. Request validation!

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -41,7 +41,11 @@ class Auth:
         Returns:
         None for now (will be implemented later)
         """
-        return None
+        if request is None:
+            return None
+        if request.headers.get('Authorization') is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
