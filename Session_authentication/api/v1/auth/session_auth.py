12781286858a03d0f1aa27a4fc7762c98875c19f
LucasNGRE <lucas12negre@gmail.com>
2025-06-23 15:07:04 +0200

    Add current_user method to SessionAuth class for retrieving the current user based on session ID

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """Module for basic authentication using Flask."""
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -43,3 +44,23 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id, None)
+
+    def current_user(self, request=None):
+        """
+        Retrieves the current user based on the session ID.
+
+        Args:
+            request: The Flask request object.
+
+        Returns:
+            returns a User instance based on a cookie value.
+        """
+        if request is None:
+            return None
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+        return User.get(user_id)
