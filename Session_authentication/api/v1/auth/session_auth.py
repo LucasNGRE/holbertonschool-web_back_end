f9fd7c6e1a75f8f64b971190e073d0ac7f7b01f7
LucasNGRE <lucas12negre@gmail.com>
2025-06-23 11:10:13 +0200

    Add user_id_for_session_id method to SessionAuth class

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -29,3 +29,17 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())  # Generate session ID like id in Base
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Retrieves the user ID associated with a session ID.
+
+        Args:
+            session_id (str): The session ID.
+
+        Returns:
+            str: The user ID associated with the session ID.
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id, None)
