44e1e48a8dc835bd2852e566f25856b09c940cd5
LucasNGRE <lucas12negre@gmail.com>
2025-06-23 10:15:20 +0200

    Add SessionAuth class for session-based authentication

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module for basic authentication using Flask."""
+from api.v1.auth.auth import Auth
+
+class SessionAuth(Auth):
+    """ For the moment this class will be empty.
+    It’s the first step for creating a new
+    authentication mechanism"""
+    def __init__(self):
+        """Initialize the SessionAuth class."""
+        super().__init__()
+        # You can add any initialization code here if needed
+        # For now, it just calls the parent class constructor
+        pass
