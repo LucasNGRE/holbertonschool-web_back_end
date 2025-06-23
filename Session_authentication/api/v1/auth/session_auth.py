b40d6b58aadee26a53a26dfa43853f20a32ffed4
LucasNGRE <lucas12negre@gmail.com>
2025-06-23 10:32:17 +0200

    Refactor SessionAuth class to implement session creation method

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python3
 """Module for basic authentication using Flask."""
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
-    """ For the moment this class will be empty.
-    It’s the first step for creating a new
-    authentication mechanism"""
+    """
+    Session authentication class that extends the Auth class.
+    """
+    user_id_by_session_id = {}  # New class attribute
+
     def __init__(self):
-        """Initialize theSessionAuth class."""
+        """Initialize SessionAuth."""
         super().__init__()
-        # You can add any initialization code here if needed
-        # For now, it just calls the parent class constructor
-        pass
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a session ID for a user.
+
+        Args:
+            user_id (str): The ID of the user.
+
+        Returns:
+            str: The session ID.
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())  # Generate session ID like id in Base
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
