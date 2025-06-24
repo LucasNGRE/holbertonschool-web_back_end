5f48e0a7cbc061b3d276c0488c168648d678d896
LucasNGRE <lucas12negre@gmail.com>
2025-06-24 09:24:50 +0200

    Refactor logout route decorator for improved readability

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -42,8 +42,9 @@ def session_auth_login() -> str:
     return response
 
 
-@app_views.route('/auth_session/logout', methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route(
+    '/auth_session/logout', methods=['DELETE'], strict_slashes=False
+)
 def session_auth_logout() -> str:
     """
     DELETE /api/v1/auth_session/logout
