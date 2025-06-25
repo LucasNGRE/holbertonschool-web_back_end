db5e6f599753f8d0d75048dffb2631d351cc7284
LucasNGRE <lucas12negre@gmail.com>
2025-06-25 09:36:39 +0200

    pycodestyle

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class User(Base):
     """User class mapped to the 'users' table."""
     __tablename__ = 'users'
