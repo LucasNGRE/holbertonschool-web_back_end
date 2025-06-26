96f1e73e70c70d402d3ceefb4e557e72195c38a3
LucasNGRE <lucas12negre@gmail.com>
2025-06-26 10:26:34 +0200

    Add reset_password_token method to generate and store password reset tokens

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -75,3 +75,14 @@ class Auth:
             self._db.update_user(user.id, session_id=None)
         except NoResultFound:
             pass
+
+    def reset_password_token(self, email: str) -> str:
+        """Generate a password reset token for a user."""
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            user.reset_token = reset_token
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            return None
