eb6c8b8c0551d4d2479765ba63f1ca58e384c98a
LucasNGRE <lucas12negre@gmail.com>
2025-06-26 09:50:16 +0200

    Add UUID generation method to Auth class

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -5,6 +5,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -36,3 +37,7 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
+
+    def _generate_uuid(self) -> str:
+        """Generate a UUID for the user"""
+        return str(uuid.uuid4())
