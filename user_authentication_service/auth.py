dcbc6c2cd5977e2972b805c140ecf94a3300057c
LucasNGRE <lucas12negre@gmail.com>
2025-06-26 10:28:12 +0200

    Fix reset_password_token method to raise ValueError on user not found

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -80,9 +80,8 @@ class Auth:
         """Generate a password reset token for a user."""
         try:
             user = self._db.find_user_by(email=email)
-            reset_token = _generate_uuid()
-            user.reset_token = reset_token
-            self._db.update_user(user.id, reset_token=reset_token)
-            return reset_token
         except NoResultFound:
-            return None
+            raise ValueError
+        token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=token)
+        return token
