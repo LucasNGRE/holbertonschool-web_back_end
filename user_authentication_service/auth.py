5bc901cc169a079e7c5ffd3c6ddbb299d93bbf6f
LucasNGRE <lucas12negre@gmail.com>
2025-06-26 09:51:30 +0200

    Add UUID generation function to improve user registration process

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -13,6 +13,11 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
+def _generate_uuid() -> str:
+    """Generate a UUID for the user"""
+    return str(uuid.uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -37,7 +42,3 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
-
-    def _generate_uuid(self) -> str:
-        """Generate a UUID for the user"""
-        return str(uuid.uuid4())
