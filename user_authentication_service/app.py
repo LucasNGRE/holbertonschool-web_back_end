6f65fcd2f433e91a8ef92af62cf816638152b4a1
LucasNGRE <lucas12negre@gmail.com>
2025-06-26 10:14:08 +0200

    Add login route to authenticate users and create sessions

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """App module
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 import bcrypt
 from user import User
@@ -33,6 +33,19 @@ def register_user():
         return jsonify({"email": user.email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """Logs user in by verifying credentials and creating session."""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not AUTH.valid_login(email, password):
+       abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie('session_id', session_id)
+    return response
 
 
 if __name__ == '__main__':
