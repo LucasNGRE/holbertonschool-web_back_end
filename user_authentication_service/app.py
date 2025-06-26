cfe95b4cd9e82e9e2a5bd02f98dbe2cc23963078
LucasNGRE <lucas12negre@gmail.com>
2025-06-26 09:29:18 +0200

    Pycode

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -14,6 +14,8 @@ def home():
 
 AUTH = Auth()
 @app.route('/users', methods=['POST'])
+
+
 def register_user():
     """Register a new user"""
     email = request.form.get('email')
@@ -27,6 +29,7 @@ def register_user():
         return jsonify({"email": user.email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
