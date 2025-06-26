b30bec730044014d76f55efd3f972a8fc115f3fb
LucasNGRE <lucas12negre@gmail.com>
2025-06-26 10:19:23 +0200

    Implement destroy_session method to clear user session ID

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -64,3 +64,14 @@ class Auth:
             return self._db.find_user_by(session_id=session_id)
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy the session of a user by setting session_id to None."""
+        if user_id is None:
+            return
+        try:
+            user = self._db.find_user_by(id=user_id)
+            user.session_id = None
+            self._db.update_user(user.id, session_id=None)
+        except NoResultFound:
+            pass
