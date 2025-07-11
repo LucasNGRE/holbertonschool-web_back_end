61046c6964c194ce9dada7839a64c362274373f9
LucasNGRE <lucas12negre@gmail.com>
2025-07-11 15:07:05 +0200

    Add initial Flask app and HTML template for i18n

---
+++ i18n/0-app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""flask app for i18n
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('0-index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
