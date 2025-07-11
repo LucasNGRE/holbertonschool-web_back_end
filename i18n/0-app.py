9c8be5ad0ffaf1dada3a4351727381e52628f1f8
LucasNGRE <lucas12negre@gmail.com>
2025-07-11 15:09:01 +0200

    Add whitespace for improved readability in 0-app.py

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -6,9 +6,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def index():
+    """Render the index.html template for the root route."""
     return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
